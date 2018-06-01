f45fb35d4a3992b255c7d6a267cdd8702869b891
Denis Glavatskikh <denis.glavatskikh@appodeal.com>
2018-06-01 10:18:20 +0300

    added consentInformation validation logic and the display of the consentDialog
    (cherry picked from commit 89d55a4)

---
+++ app/src/main/java/com/appodeal/test/SplashActivity.java
@@ -0,0 +1,46 @@
+package com.appodeal.test;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.ads.consent.ConsentInfoUpdateListener;
+import com.google.ads.consent.ConsentInformation;
+import com.google.ads.consent.ConsentStatus;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ConsentInformation consentInformation = ConsentInformation.getInstance(this);
+        /*
+        Requesting Consent from European Users
+        https://developers.google.com/admob/android/eu-consent
+        IMPORTANT: YOU MUST SPECIFY YOUR PUBLISHER_IDS
+        HERE A TEST PUBLISHERIDS
+         */
+        String[] publisherIds = {"pub-0123456789012345"};
+        consentInformation.requestConsentInfoUpdate(publisherIds, new ConsentInfoUpdateListener() {
+            @Override
+            public void onConsentInfoUpdated(ConsentStatus consentStatus) {
+                if (ConsentInformation.getInstance(SplashActivity.this).isRequestLocationInEeaOrUnknown()) {
+                    startActivity(new Intent(SplashActivity.this, GDPRActivity.class));
+                } else {
+                    startMainActivityWithDefaultConsent();
+                }
+            }
+
+            @Override
+            public void onFailedToUpdateConsentInfo(String errorDescription) {
+                startMainActivityWithDefaultConsent();
+            }
+        });
+    }
+
+    private void startMainActivityWithDefaultConsent() {
+        startActivity(MainActivity.getIntent(this, true));
+    }
+}
